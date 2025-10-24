73f43a15a3ffd377a5cc6b2c6094ce4fda35dc7c
Vazy <haiminh454@yahoo.com>
2025-10-24 16:00:07 +0700

    improve update product

--- server/src/routes/productsRoute/productsRoute.js
+++ server/src/routes/productsRoute/productsRoute.js
@@ -6,7 +6,7 @@ const {auth} = require('../../middlewares/auth');
 const upload = require('../../middlewares/uploadImage');
 
 router.route('/products/add').post(validateAccessToken,auth(["admin"]),upload.array('images', 5),addProduct);
-router.route('/products/:id').put(validateAccessToken,auth(["admin"]),updateProduct);
+router.route('/products/:id').put(validateAccessToken,auth(["admin"]),upload.array('images', 5),updateProduct);
 router.route('/products/:id').delete(validateAccessToken,auth(["admin"]),deleteProduct);
 router.route('/products/:id').get(validateAccessToken,auth(["admin"]),getProductById);
 router.get('/products', getAllProducts);
